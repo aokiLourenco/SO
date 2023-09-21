e06ee888aee746beb54f8d8a12d0be52d54030ff
Aoki <aaokilourenco@ua.pt>
2023-09-21 13:00:05 +0100

    bruh

---
+++ SO-P/02-1-processes/code/kysnigger.cpp
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+#include "delays.h"
+#include "process.h"
+
+int main (void)
+{
+    int counter = 1;
+
+    pid_t ret = pfork();
+    
+    if (ret != 0)
+    {
+        for (int i = 1; i<=10; i++)
+        {
+            printf("%d, PID = %d, PPID = %d\n", counter, getpid(), getppid());
+            counter++;
+        }
+    } else { // counter from 10 to 20
+        for (int i = 1; i<=10; i++)
+        {
+            printf("%d, PID = %d, PPID = %d\n", counter+10, getpid(), getppid());
+            counter++;
+        }
+    }
+
+    return EXIT_SUCCESS;
+}
